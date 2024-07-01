6f50d5e683321bdcaf0dead08d47891c0accad04
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-07-01 17:21:45 +0530

    Workingg Finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './pages/Hero/Hero'
 import Navbar from "./pages/Navbar/Navbar.jsx"
 import Application from "./pages/Application/Application"
 import Working from "./pages/Working/Working"
+import Portfolio from "./pages/Portfolio/Portfolio"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Hero />
       <Application />
       <Working />
+      <Portfolio />
 
     </>
   )
