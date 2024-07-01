b1b6353344f508eaa75445c0994b5f4fd7c8c85b
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-07-01 15:35:30 +0530

    dfsd

--- src/pages/Application/Application.jsx
+++ src/pages/Application/Application.jsx
@@ -11,7 +11,7 @@ import C6 from "../../../public/c6.png"
 function Application() {
   return (
     <>
-        <div className="application-container  pb-2">
+        <div className="application-container  pb-16">
             <p className="mx-6 xl:mx-0 font-bold sm:text-2xl">Application areas </p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-3">
                 
--- src/pages/Working/Working.jsx
+++ src/pages/Working/Working.jsx
@@ -9,20 +9,56 @@ function Working() {
     })
   return (
     <>
-    <div className="working-color  relative z-0 pt-[px] ">
-            <div className="w-yellow"></div>
-            <div className="w-red"></div>
-            <div className="w-purple"></div>
-            <div className="w-blue"></div>
-        </div>
-    <div className="working-container pb-[200px] absolute z-1">
-        
+    <div className="working-container  pb-[200px] ">
         <div className="">
-            <p>How We are working?</p>
+        <p className="mx-6 font-bold text-lg lg:mx-0">How We are Working</p>
         </div>
+        
+        <div className="flex justify-center items-center  ">
+            <div className="box1 absolute z-10  grid grid-cols-1 gap-4  sm:grid sm:grid-cols-3 mt-[250px]" >
+                <div className="flex justify-center items-center  bg-white px-[80px] py-5">
+                    <p className="text-center ">Nisal Shiranda</p>
+                </div>
+                <div className="flex justify-center items-center  bg-white px-[80px] py-5">
+                    <p className="text-center ">Nisal Shiranda</p>
+                </div>
+                <div className="flex justify-center items-center  bg-white px-[80px] py-5">
+                    <p className="text-center ">Nisal Shiranda</p>
+                </div>
+                
+                
+            </div>
+            
+        </div>
+
+        <div className="flex justify-center items-center  ">
+            <div className="box1 absolute z-10  grid grid-cols-1 gap-4  sm:grid sm:grid-cols-2 mt-[450px]" >
+                <div className="flex justify-center items-center  bg-white px-[80px] py-5">
+                    <p className="text-center ">Nisal Shiranda</p>
+                </div>
+                <div className="flex justify-center items-center  bg-white px-[80px] py-5">
+                    <p className="text-center ">Nisal Shiranda</p>
+                </div>
+               
+                
+                
+            </div>
+            
+        </div>
+      
+
+        <div className="working-color  relative z-0 pt-[px] mt-[50px] md:mt-[80px] ">
+                <div className="w-yellow"></div>
+                <div className="w-red"></div>
+                <div className="w-purple"></div>
+                <div className="w-blue"></div>
+        </div>
+        
+        
         {/* <p className="font-bold text-xl">How We are working?</p>
         <div className=" mt-[50px] w-[200px] h-[200px] ml-10 bg-black" data-aos="fade-right"></div> */}
     </div>
+    
     </>
   )
 }
