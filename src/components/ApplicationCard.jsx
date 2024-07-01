5418d41301d0c58c322d4f43998adebe0d02bb33
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-07-01 10:07:13 +0530

    dcds

--- src/components/ApplicationCard.jsx
+++ src/components/ApplicationCard.jsx
@@ -3,11 +3,11 @@ import C1 from "../../public/c1.png"
 
 function ApplicationCard () {
   return (
-    <div className="card-container flex flex-col justify-center items-center">
+    <div className="card-container flex flex-col justify-center items-center mx-6">
         <div className="card-image relative">
-            <img className="relative top-[40px]" src={C1} alt="placeholder"></img>
+            <img className="cardImage" src={C1} alt="placeholder"></img>
         </div>
-        <div className="card-text flex flex-col px-1 pt-8 pb-3 bg-white rounded ">
+        <div className="card-text flex flex-col px-4 pt-8 pb-3 bg-white rounded ">
             <p className="text-center font-semibold">Automation and optimization of operational activities</p>
             <p className="text-center font-light">Working staff, HR, Top management</p>
         </div>
