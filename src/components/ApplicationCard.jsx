802cea244f157413b9be4b259988d84074c7e675
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-07-01 10:38:34 +0530

    Application Section Finished

--- src/components/ApplicationCard.jsx
+++ src/components/ApplicationCard.jsx
@@ -1,20 +1,34 @@
 import "./ApplicationCard.css"
-import C1 from "../../public/c1.png"
+import PropTypes from "prop-types"
 
-function ApplicationCard () {
+
+export const ApplicationCard = ({
+    imgSrc,
+    heading,
+    desc,
+}) => {
   return (
     <div className="card-container flex flex-col justify-center items-center mx-6">
         <div className="card-image relative">
-            <img className="cardImage" src={C1} alt="placeholder"></img>
+            <img className="cardImage" src={imgSrc} alt="placeholder"></img>
         </div>
-        <div className="card-text flex flex-col px-4 pt-8 pb-3 bg-white rounded ">
-            <p className="text-center font-semibold">Automation and optimization of operational activities</p>
-            <p className="text-center font-light">Working staff, HR, Top management</p>
+        <div className="card-text flex flex-col px-4 pt-8 pb-3 bg-[#EEF7FF] rounded-xl ">
+            <p className="text-center font-semibold">{heading}</p>
+            <p className="text-center font-light">{desc}</p>
         </div>
     </div>
   )
 }
 
-
-
 export default ApplicationCard
+
+ApplicationCard.propTypes = {
+
+    imgSrc: PropTypes.string,
+    heading: PropTypes.string,
+    desc: PropTypes.string,
+   
+ 
+   };
+
+
--- src/pages/Application/Application.jsx
+++ src/pages/Application/Application.jsx
@@ -1,17 +1,74 @@
 import "./Application.css"
 import Card from "../../components/ApplicationCard"
+import C1 from "../../../public/c1.png"
+import C2 from "../../../public/c2.png"
+import C3 from "../../../public/c3.png"
+import C4 from "../../../public/c4.png"
+import C5 from "../../../public/c5.png"
+import C6 from "../../../public/c6.png"
+
 
 function Application() {
   return (
     <>
-        <div className="application-container">
-            <p className="mx-6 xl:mx-0 font-bold">Application areas </p>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-4">
-                <div className="flex justify-center items-center"><Card /></div>
-                <div className="flex justify-center items-center" ><Card /></div>
-                <div className="flex justify-center items-center"><Card /></div>
+        <div className="application-container  pb-2">
+            <p className="mx-6 xl:mx-0 font-bold sm:text-2xl">Application areas </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-3">
+                
+                <div className="flex justify-center items-center">
+                <Card
+                    imgSrc={C1}
+                    heading="Automation and optimization of operational activities"
+                    desc="Working staff, HR, Top management"
+                 />
+
+                 </div>
+
+                 <div className="flex justify-center items-center">
+                <Card
+                    imgSrc={C2}
+                    heading="Marketing tool"
+                    desc="Point of contact. Brand extension. Increased customer loyalty."
+                 />
+
+                 </div>
+                 <div className="flex justify-center items-center">
+                <Card
+                    imgSrc={C3}
+                    heading="Professional tool"
+                    desc="A tool for use in professional activities and be half of IT"
+                 />
+
+                 </div>
+                 
+                 <div className="flex justify-center items-center">
+                <Card
+                    imgSrc={C4}
+                    heading="Sales"
+                    desc="One-click sales. Sales funnel expansion. Loyalty program. NFC. QR, Bar codes"
+                 />
+
+                 </div>
+                 <div className="flex justify-center items-center">
+                <Card
+                    imgSrc={C5}
+                    heading="Social networks. Games"
+                    desc="Formation of a client base, communities, fans. bases, etc."
+                 />
+
+                 </div>
+                 <div className="flex justify-center items-center">
+                <Card
+                    imgSrc={C6}
+                    heading="IoT"
+                    desc="Remote control of devices and complex systems. Wi-Fi, Bluetooth"
+                 />
+
+                 </div>
             </div>
-        </div>
+            </div>
+        
+       
     </>
   )
 }
